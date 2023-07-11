87eb7b70fd8657a0a30caedd6fe79ad5c5885978
johnathost <contact@yannisvoyatzis.com>
2023-07-12 00:29:12 +0300

    Update Confluence_Pre-Job.ps1
    - Added comments.
    - Made some position improvements.
    - Removed clutter commands.
    - Fixed some execution problems.
    - Improved output dialog.

--- PowerShell/Confluence_Pre-Job.ps1
+++ PowerShell/Confluence_Pre-Job.ps1
@@ -17,91 +17,104 @@ Generate an API token: https://confluence.atlassian.com/cloud/api-tokens-9388396
 ---------------------------------------------------------------------------------------------
 #>
 
-#Edit the following variables to access your cloud site.
-$account     = 'youratlassianconfluence' # Atlassian subdomain i.e. whateverproceeds.atlassian.net
-$CredentialName = 'Atlassian' # The name on Windows Credential Manager
+# Edit the following variables to access your cloud site.
+$account     = 'youratlassianconfluence' # Atlassian subdomain i.e. whateverproceeds.atlassian.net.
+$CredentialName = 'Atlassian' # The name on Windows Credential Manager.
 $destination = 'C:\VeeamStaging\Confluence' # Location on server where script is run to dump the backup zip file.
-$attachments = 'true' # Tells the script whether or not to pull down the attachments as well
-$cloud     = 'true' # Tells the script whether to export the backup for Cloud or Server
+$attachments = 'true' # Tells the script whether or not to pull down the attachments as well.
+$cloud     = 'true' # Tells the script whether to export the backup for Cloud or Server.
 $today       = Get-Date -format yyyyMMdd-hhm
+$BackupFilename = "confluence-backup-$today.zip" # The confluence backup file name.
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 
-if(!(Test-Path -path $destination)){
-write-host "Folder is not present, creating folder"
-mkdir $destination #Make the path and folder is not present
-}
-else{
-write-host "Path is already present"
+if ($PSVersionTable.PSVersion.Major -lt 4) {
+    Write-Error "Script requires at least PowerShell version 4. Get it here: https://www.microsoft.com/en-us/download/details.aspx?id=40855"
 }
 
 Start-Transcript -Append $destination\ConfluenceBU.log
 
+if(!(Test-Path -path $destination)){
+    Write-Host "Folder is not present, creating folder"
+    mkdir $destination
+}
+else{
+    Write-Host "Path is already present"
+}
+
+# Get the credentials from Windows Credential Manager
 $Cred = Get-StoredCredential -Target $CredentialName
 $ATLUser = $Cred.UserName.ToString()
 $auth = [System.Text.Encoding]::UTF8.GetBytes(("{0}:{1}" -f $ATLUser,([Runtime.InteropServices.Marshal]::PtrToStringBSTR([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Cred.Password)))))
 $auth = [System.Convert]::ToBase64String($auth)
 
+# Set authentication headers
+$header = @{
+    "Authorization" = "Basic "+$auth
+    "Content-Type"="application/json"
+    "Accept" = "application/json"
+    }
+
+# Set body
 $body = @{
           cbAttachments=$attachments
           exportToCloud=$cloud
          }
 $bodyjson = $body | ConvertTo-Json
 
-if ($PSVersionTable.PSVersion.Major -lt 4) {
-    throw "Script requires at least PowerShell version 4. Get it here: https://www.microsoft.com/en-us/download/details.aspx?id=40855"
-}
+# Set backup success message
+$BackupSuccessMessage = @"
+Confluence backup for $account has been completed.
+Backup location: $destination
+Backup File: $BackupFilename
+"@
 
-# Create header for authentication
-    [string]$ContentType = "application/json"
-    [string]$URI = "https://$account.atlassian.net/wiki/rest/obm/1.0/runbackup"
+# Set Atlassian URLs
+$BackupEndpointURL = "https://$account.atlassian.net/wiki/rest/obm/1.0/runbackup"
+$BackupsStatusURL = "https://$account.atlassian.net/wiki/rest/obm/1.0/getprogress"
 
-    #Create Header
-        $header = @{
-                "Authorization" = "Basic "+$auth
-                "Content-Type"="application/json"
-                    }
-
-# Request backup
+# Create a Confluence Cloud backup
 try {
-        $InitiateBackup = Invoke-RestMethod -Method Post -Headers $header -Uri $URI -ContentType $ContentType -Body $bodyjson -Verbose | ConvertTo-Json -Compress | Out-Null
+    $BackupResponse = Invoke-RestMethod -Method POST -Uri $BackupEndpointURL -Headers $header -Body $bodyjson
 } catch {
-        $InitiateBackup = $_.Exception.Response.GetResponseStream()
-        $reader = New-Object System.IO.StreamReader($InitiateBackup)
-        $reader.BaseStream.Position = 0
-        $reader.DiscardBufferedData()
-        $responseBody = $reader.ReadToEnd();
+    $BackupResponse = $_.Exception.Response.GetResponseStream()
+    $reader = New-Object System.IO.StreamReader($BackupResponse)
+    $reader.BaseStream.Position = 0
+    $reader.DiscardBufferedData()
+    $ResponseBody = $reader.ReadToEnd();
 }
 
-$responseBody
+if ($BackupResponse.StatusCode -eq 200) {
+    Write-Host $ResponseBody
+} else {
+    Write-Warning $ResponseBody
+}
 
-$GetBackupID = Invoke-WebRequest -Method Get -Headers $header -UseBasicParsing https://$account.atlassian.net/wiki/rest/obm/1.0/getprogress
+# Get Backup information
+$GetBackupID = Invoke-WebRequest -Method Get -Headers $header -UseBasicParsing $BackupsStatusURL
 $BackupStatus = convertfrom-json $GetBackupID.content
-$BackupStatus.alternativePercentage
-$BackupStatus.filename
 
 # Wait for backup to finish
 do {
-    $status = Invoke-RestMethod -Method Get -Headers $header -Uri "https://$account.atlassian.net/wiki/rest/obm/1.0/getprogress"
-        $BackupStatus = convertfrom-json $GetBackupID.content
-        $BackupStatus.alternativePercentage
-    $statusoutput = $BackupStatus.alternativePercentage
-    $s
+    $status = Invoke-RestMethod -Method Get -Headers $header -Uri $BackupsStatusURL
+    $BackupStatus = convertfrom-json $GetBackupID.content
+    $StatusOutput = $BackupStatus.alternativePercentage
 
-    if ($statusoutput -eq "100%") {
-        $percentage = "100"
-
-        Write-Progress -Activity 'Creating backup' -Status $statusoutput -PercentComplete $percentage
+    if ($StatusOutput -eq "100%") {
+        Write-Host "Creating backup, please wait..."
     }
+
     Start-Sleep -Seconds 5
-} while($statusoutput -ne '100%')
+} while($StatusOutput -ne '100%')
 
-# Download
+# Download Backup
 if ([bool]($status.PSObject.Properties.Name -match "failedMessage")) {
-    throw $status.failedMessage
-}
-   
-                     
-$FileName = $BackupStatus.filename
-$DownloadURI = "https://$account.atlassian.net/wiki/download/$FileName"
+    Write-Error $status.failedMessage
+} else {
+    $BackupLocation = $BackupStatus.filename
+    $DownloadURI = "https://$account.atlassian.net/wiki/download/$BackupLocation"
 
-Invoke-WebRequest -Method Get -Headers $header -UseBasicParsing -WebSession $session -Uri $DownloadURI -OutFile (Join-Path -Path $destination -ChildPath "confluence-backup-$today.zip")
+    Write-Host "Writing Backup contents..."
+    Invoke-WebRequest -Method Get -Headers $header -UseBasicParsing -Uri $DownloadURI -OutFile (Join-Path -Path $destination -ChildPath $BackupFilename)
+    Write-Host $BackupSuccessMessage
+}
